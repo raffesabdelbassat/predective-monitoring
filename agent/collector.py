159981472bd0ca0b21e04a0cf1e816a2ea74216c
Your Name <you@example.com>
2026-09-18 16:55:11 +0200

    agent now runs continously , sendin metrics every 10 sec

--- agent/collector.py
+++ agent/collector.py
@@ -19,3 +19,33 @@ def send_metrics(metrics: dict):
 if __name__ == "__main__":
     metrics = collect_metrics()
     send_metrics(metrics) 
+
+    import psutil
+import requests
+import time
+
+BACKEND_URL = "http://127.0.0.1:8000/metrics"
+COLLECTION_INTERVAL_SECONDS = 10
+
+def collect_metrics() -> dict:
+    cpu_percent = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory()
+
+    return {
+        "cpu_percent": cpu_percent,
+        "memory_percent": memory.percent,
+    }
+
+def send_metrics(metrics: dict):
+    response = requests.post(BACKEND_URL, json=metrics, timeout=5)
+    print(f"Sent metrics, backend responded: {response.json()}")
+
+def run():
+    print("Agent started. Press Ctrl+C to stop.")
+    while True:
+        metrics = collect_metrics()
+        send_metrics(metrics)
+        time.sleep(COLLECTION_INTERVAL_SECONDS)
+
+if __name__ == "__main__":
+    run() 
